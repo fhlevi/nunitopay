d5da3c869daa8453c47ab885f5cc8a6eaacd96d0
Levi <faisal@sakoo.id>
2021-11-28 21:11:48 +0700

    feat(meta): change name title and icon favicon

--- src/components/navigation/MetaHeader.js
+++ src/components/navigation/MetaHeader.js
@@ -1,13 +1,13 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import imgFavicon from 'assets/images/icons/Favicon-Sooltan.png'
+import imgFavicon from 'assets/images/icons/favicon.ico'
 
 function MetaHeader() {
     return ( 
         <>
             <HelmetProvider>
                 <Helmet>
-                    <title>sooltanPay</title>
-                    <link rel="icon" type="image/png" sizes="32x32" href={imgFavicon} />
+                    <title>nunitoPay</title>
+                    <link rel="icon" type="image/ico" sizes="32x32" href={imgFavicon} />
                 </Helmet>
             </HelmetProvider>
         </>
